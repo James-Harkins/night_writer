4c2736d2e8da3a4315d1b6ca5fd9aba3ee7c3fdf
James Harkins <jh.harkins@protonmail.com>
2022-03-04 14:59:21 -0700

    test: create test for dictionary english_to_braille method

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Dictionary do
     it 'is initialized with an alphabet to braille hash' do
       expect(@dictionary.alphabet).to be_a(Hash)
       expect(@dictionary.alphabet["a"]).to be_a(Array)
+      expect(@dictionary.alphabet["a"]).to eq([[0, "."], [".", "."], [".", "."]])
       expect(@dictionary.alphabet.all? {|key, value| value.length == 3}).to be(true)
     end
 
@@ -18,5 +19,8 @@ RSpec.describe Dictionary do
       expected = (@dictionary.alphabet.keys.count == @dictionary.alphabet.keys.uniq.count)
       expect(expected).to be(true)
     end
+
+    it 'can convert letters to braille' do
+      expect(@dictionary.english_to_braille("a")).to eq([[0, "."], [".", "."], [".", "."]])
   end
 end
