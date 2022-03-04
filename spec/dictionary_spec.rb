63ea5287406fbb6edb47f0c1399cee558dc3386f
James Harkins <jh.harkins@protonmail.com>
2022-03-04 11:17:58 -0700

    feat: dictionary class passing initialize tests

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -4,13 +4,18 @@ require 'pry'
 RSpec.describe Dictionary do
 
   before(:each) do
-    dictionary = Dictionary.new
+    @dictionary = Dictionary.new
   end
 
   describe '#initialze' do
     it 'is initialized with an alphabet to braille hash' do
       expect(@dictionary.alphabet).to be_a(Hash)
       expect(@dictionary.alphabet["a"]).to be_a(Matrix)
+    end
+
+    it 'has unique keys' do
+      expected = (@dictionary.alphabet.keys.count == @dictionary.alphabet.keys.uniq.count)
+      expect(expected).to be(true)
     end
   end
 end
