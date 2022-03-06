5c6ee102d97cf17cad61a44a95ecc6b38e64d1c1
James Harkins <jh.harkins@protonmail.com>
2022-03-06 10:05:13 -0700

    test: create dictionary class test for braille_to_english method

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -19,9 +19,17 @@ RSpec.describe Dictionary do
       expected = (@dictionary.alphabet.keys.count == @dictionary.alphabet.keys.uniq.count)
       expect(expected).to be(true)
     end
+  end
 
+  describe '#english_to_braille' do
     it 'can convert letters to braille' do
       expect(@dictionary.english_to_braille("a")).to eq([[0, "."], [".", "."], [".", "."]])
+    end
+  end
+
+  describe '#braille_to_english' do
+    it 'can convert braille to english' do
+      expect(@dictionary.braille_to_english([[0, "."], [".", "."], [0, "."]])).to eq("k")
     end
   end
 end
