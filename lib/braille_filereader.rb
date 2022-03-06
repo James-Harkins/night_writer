2eabb18058baae632053209cb713c2f5841ba908
James Harkins <jh.harkins@protonmail.com>
2022-03-06 11:36:13 -0700

    feat: braille_filereader class consolidate_lines method passing test

--- lib/braille_filereader.rb
+++ lib/braille_filereader.rb
@@ -21,4 +21,28 @@ class BrailleFileReader
     text = read(filename)
     text.map {|line| line.length}.sum / 6
   end
+
+  def consolidate_lines
+    consolidated_lines = []
+    top_row_number = 3
+    middle_row_number = 4
+    bottom_row_number = 5
+    text = read(@filename)
+    consolidated_lines << text[0]
+    consolidated_lines << text[1]
+    consolidated_lines << text[2]
+    while text[top_row_number]
+      consolidated_lines[0] << text[top_row_number]
+      top_row_number += 3
+    end
+    while text[middle_row_number]
+      consolidated_lines[1] << text[middle_row_number]
+      middle_row_number += 3
+    end
+    while text[bottom_row_number]
+      consolidated_lines[2] << text[bottom_row_number]
+      bottom_row_number += 3
+    end
+    consolidated_lines
+  end
 end
