8f43978fca5bf4fee4392e73ca59cfa77c3944ee
James Harkins <jh.harkins@protonmail.com>
2022-03-06 15:52:27 -0700

    refactor: refactor braille_filereader consolidate_lines method

--- lib/braille_filereader.rb
+++ lib/braille_filereader.rb
@@ -23,22 +23,26 @@ class BrailleFileReader
   end
 
   def consolidate_lines
-    consolidated_lines = []
-    top_row_number = 3
-    middle_row_number = 4
-    bottom_row_number = 5
+    consolidated_lines = ["", "", ""]
     text = read(@filename)
-    consolidated_lines << text[0]
-    consolidated_lines << text[1]
-    consolidated_lines << text[2]
-    while text[top_row_number] && text[middle_row_number] && text[bottom_row_number]
-      consolidated_lines[0] << text[top_row_number]
-      top_row_number += 3
-      consolidated_lines[1] << text[middle_row_number]
-      middle_row_number += 3
-      consolidated_lines[2] << text[bottom_row_number]
-      bottom_row_number += 3
-    end
+    text.each_with_index {|line, index| consolidated_lines[index % 3] << line}
+    
+    # consolidated_lines = []
+    # top_row_number = 3
+    # middle_row_number = 4
+    # bottom_row_number = 5
+    # text = read(@filename)
+    # consolidated_lines << text[0]
+    # consolidated_lines << text[1]
+    # consolidated_lines << text[2]
+    # while text[top_row_number] && text[middle_row_number] && text[bottom_row_number]
+    #   consolidated_lines[0] << text[top_row_number]
+    #   top_row_number += 3
+    #   consolidated_lines[1] << text[middle_row_number]
+    #   middle_row_number += 3
+    #   consolidated_lines[2] << text[bottom_row_number]
+    #   bottom_row_number += 3
+    # end
     consolidated_lines
   end
 end
