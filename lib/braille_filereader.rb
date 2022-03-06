6b07f0d27ef7623f4d3374a89eb58c7968edf18f
James Harkins <jh.harkins@protonmail.com>
2022-03-06 16:46:56 -0700

    feat: pass braille_reader convert_to_english method tests

--- lib/braille_filereader.rb
+++ lib/braille_filereader.rb
@@ -27,22 +27,6 @@ class BrailleFileReader
     text = read(@filename)
     text.each_with_index {|line, index| consolidated_lines[index % 3] << line}
     consolidated_lines
-    # consolidated_lines = []
-    # top_row_number = 3
-    # middle_row_number = 4
-    # bottom_row_number = 5
-    # text = read(@filename)
-    # consolidated_lines << text[0]
-    # consolidated_lines << text[1]
-    # consolidated_lines << text[2]
-    # while text[top_row_number] && text[middle_row_number] && text[bottom_row_number]
-    #   consolidated_lines[0] << text[top_row_number]
-    #   top_row_number += 3
-    #   consolidated_lines[1] << text[middle_row_number]
-    #   middle_row_number += 3
-    #   consolidated_lines[2] << text[bottom_row_number]
-    #   bottom_row_number += 3
-    # end
   end
 
   def convert_to_braille_characters
@@ -59,7 +43,12 @@ class BrailleFileReader
       column_1_counter += 2
       column_2_counter += 2
     end
-    binding.pry
     braille_characters
+  end
+
+  def convert_to_english
+    convert_to_braille_characters.map do |braille_character|
+      @dictionary.braille_to_english(braille_character)
+    end
   end
 end
