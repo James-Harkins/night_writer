13369f1dadb0dbd6700ee358d6bcd174b6644b2e
James Harkins <jh.harkins@protonmail.com>
2022-03-07 15:30:08 -0700

    refactor: refactor night_reader.rb

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,6 +1,8 @@
 require_relative '../lib/braille_filereader'
+require_relative '../lib/english_filereader'
 
-filereader = BrailleFileReader.new(ARGV[0])
-character_count = filereader.count_characters(ARGV[0])
-filereader.create_new_english_file(ARGV[1])
+braille_filereader = BrailleFileReader.new(ARGV[0])
+braille_filereader.create_new_english_file(ARGV[1])
+english_filereader = EnglishFileReader.new(ARGV[1])
+character_count = english_filereader.count_characters(ARGV[1])
 puts "Created '#{ARGV[1]}' containing #{character_count} characters"
