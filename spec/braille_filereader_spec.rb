ad31c1eb0bcdbd12703594e16aad7b7ce46a9871
James Harkins <jh.harkins@protonmail.com>
2022-03-06 17:17:39 -0700

    docs: update message.txt with additional space

--- spec/braille_filereader_spec.rb
+++ spec/braille_filereader_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe BrailleFileReader do
 
   describe '#count_characters' do
     it 'can count the characters of the File object' do
-      expect(@filereader.count_characters(@filereader.filename)).to eq(155)
+      expect(@filereader.count_characters(@filereader.filename)).to eq(156)
     end
   end
 
@@ -42,7 +42,7 @@ RSpec.describe BrailleFileReader do
   describe '#convert_to_braille_characters' do
     it 'can convert consolidated_lines to an array of single braille characters' do
       expect(@filereader.convert_to_braille_characters).to be_a(Array)
-      expect(@filereader.convert_to_braille_characters.count).to eq(155)
+      expect(@filereader.convert_to_braille_characters.count).to eq(156)
       expect(@filereader.convert_to_braille_characters[0]).to be_a(Array)
     end
   end
@@ -50,7 +50,7 @@ RSpec.describe BrailleFileReader do
   describe '#convert_to_english' do
     it 'can convert an array of braille characters to an array of english characters' do
       expect(@filereader.convert_to_english).to be_a(Array)
-      expect(@filereader.convert_to_english.count).to eq(155)
+      expect(@filereader.convert_to_english.count).to eq(156)
       expect(@filereader.convert_to_english[0]).to eq("i")
     end
   end
@@ -58,7 +58,7 @@ RSpec.describe BrailleFileReader do
   describe '#convert_to_lines' do
     it 'can convert an array of english characters to strings of a max of 80 characters' do
       expect(@filereader.convert_to_lines).to be_a(Array)
-      expect(@filereader.convert_to_lines[0].length).to eq(80)
+      expect(@filereader.convert_to_lines[0].length).to eq(81)
     end
   end
 
