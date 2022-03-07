08d9832fe7513ff405da54d15923b4ad562b0d0c
James Harkins <jh.harkins@protonmail.com>
2022-03-07 14:02:44 -0700

    test: create tests for capitalization in dictionary module

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -26,11 +26,21 @@ RSpec.describe Dictionary do
     it 'can convert letters to braille' do
       expect(@filereader.english_to_braille("a")).to eq([["0", "."], [".", "."], [".", "."]])
     end
+
+    it 'supports capitalization' do
+      expected = [[".", "."], [".", "."], [".", "0"], ["0", "."], [".", "."], [".", "."]]
+      expect(@filereader.english_to_braille("A")).to eq(expected)
+    end
   end
 
   describe '#braille_to_english' do
     it 'can convert braille to english' do
       expect(@filereader.braille_to_english([["0", "."], [".", "."], ["0", "."]])).to eq("k")
+    end
+
+    it 'supports capitalization' do
+      braille = [[".", "."], [".", "."], [".", "0"], ["0", "."], [".", "."], [".", "."]]
+      expect(@filereader.english_to_braille(braille)).to eq("A")
     end
   end
 end
