f7732fd93632d4401183f2cdf63537b6529662a6
James Harkins <jh.harkins@protonmail.com>
2022-03-08 14:06:34 -0700

    test: create test for braille_filereader convert_to_braille_characters for numeric messages

--- spec/braille_filereader_spec.rb
+++ spec/braille_filereader_spec.rb
@@ -63,6 +63,11 @@ RSpec.describe BrailleFileReader do
       expect(@filereader.convert_to_braille_characters).to be_a(Array)
       expect(@filereader.convert_to_braille_characters.count).to eq(56)
       expect(@filereader.convert_to_braille_characters[0]).to be_a(Array)
+
+      numeric_filereader = BrailleFileReader.new("./number_braille.txt")
+      expect(numeric_filereader.convert_to_braille_characters).to be_a(Array)
+      expect(numeric_filereader.convert_to_braille_characters.count).to eq(16)
+      expect(numeric_filereader.convert_to_braille_characters[0]).to be_a(Array)
     end
   end
 
